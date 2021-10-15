e8839ee8d4b8b2c0aa912c84838e585fabf53a4e
Worth Lutz <worthlutz@mindspring.com>
2021-10-15 09:40:23 -0400

    changes to component and example

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import { ExampleComponent } from '@wal3/rc-hello-world'
+import { HelloWorld } from '@wal3/rc-hello-world'
 import '@wal3/rc-hello-world/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <HelloWorld text="This is a first try at a React component library. 😄" />
 }
 
 export default App
